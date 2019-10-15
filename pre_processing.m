471b93752538e7a490e502d22cf2b7bb678d09e0
YOT-ANAN <5910110270@psu.ac.th>
2019-10-16 01:25:52 +0700

    fix var and fix output path

--- pre_processing.m
+++ pre_processing.m
@@ -1,24 +1,33 @@
+clear;
 close all;
-clear all;
 
-imname='image01.jpg';
-im=imread(imname);
+disp('Preprocessing image please wait . . .');
+
+imfile='image01.jpg';
+im=imread(imfile);
+[im_path, im_name, im_ext]=fileparts(imfile);
 gim=rgb2gray(im);
-thresh=10;
-% % imout=im2bw(gim, thresh);
-bw_imout=bradley(gim, [15 15], thresh);
-figure(1),imshow(bw_imout),title('Bradley Thresholding BW image');
-imwrite (bw_imout,['./output_anisotropic/' imname 'Adapt Thresholding BW image.jpg']);
-% % open_bw_imout= bwareaopen(bw_imout, 100);
-% % imshow(open_bw_imout);
-% figure(gcf+1),imshow([mat2gray(image), bw, bw2]);
 
+% Adaptive Threshoding
+thresh=10];
+adaptive_bim=bradley(gim, [15 15], thresh);
+
+% Out Adaptive Threshoding
+figure(1),imshow(adaptive_bim),title('Adaptive Thresholding BW image');
+dst_folder = './image_adaptivethreshoding/';
+im_outname = [dst_folder,im_name,'_adaptivethreshoding',im_ext];
+imwrite (adaptive_bim,im_outname);
+
+% Anisotropic Opening
 num_iter = 5;
 delta_t = 1/15;
 kappa = 1;
-option = 1;    
-%disp('Preprocessing image please wait . . .');
-aniso_imout = anisodiff(bw_imout,num_iter,delta_t,kappa,option);
-aniso_bw_imout = im2bw(aniso_imout);
-figure(2), imshow(aniso_bw_imout), title('Anisotropic opening image');
-imwrite (aniso_bw_imout,['./output_anisotropic/' imname 'Anisotropic opening image.jpg']);
+option = 1;
+aniso_gim = anisodiff(adaptive_bim,num_iter,delta_t,kappa,option);
+aniso_bim = im2bw(aniso_gim);
+
+% Out Anisotropic
+figure(2), imshow(aniso_bim), title('Anisotropic opening image');
+dst_folder = './image_anisotropic/';
+im_outname = [dst_folder,im_name,'_anisotropic',im_ext];
+imwrite (aniso_bim,im_outname);
