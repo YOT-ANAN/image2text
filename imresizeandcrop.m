573d184c81dc5c75aa0e4e3f15b5ea1eff58af59
5910110270_Yot-ANAN <32262282+YOT-ANAN@users.noreply.github.com>
2020-03-05 14:29:16 +0700

    Create fn imresizeandcrop im input
    Complete

---
+++ imresizeandcrop.m
@@ -0,0 +1,23 @@
+function new_im = imresizeandcrop(im)
+
+   %im = imread('.\image_test\image52.jpg');
+
+    h=size(im,1);
+    w=size(im,2);
+
+    if(w>h)
+        im=imresize(im,[500 NaN]);
+    else
+        im=imresize(im,[NaN 500]);
+    end
+
+    targetSize = [500 500];
+    r = centerCropWindow2d(size(im),targetSize);
+    new_im = imcrop(im,r);
+
+
+    figure(1)
+    imshow(im)
+    figure(2)
+    imshow(new_im)
+end
